56acbd739625a286b3981e33dd7896b145e47957
DaniGarciaDev <danigarciaperez.dev@gmail.com>
2024-10-12 12:10:41 +0200

    [IMP]connect to database

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,23 @@
-/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { User } from './entities/user.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3307,                     
+      username: 'admin',    
+      password: 'admin', 
+      database: 'playtopia_db',        
+      entities: [User],                
+      synchronize: true,              
+    }),
+    TypeOrmModule.forFeature([User]), 
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
